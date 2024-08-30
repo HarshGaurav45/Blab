7cced7d928b46da1b9001b7eb0cf5e3836116b44
Harsh Gaurav <harshgaurav17@gmail.com>
2024-08-31 02:12:23 +0530

    commit

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://blab-chat.vercel.app/", {
 				query: {
 					userId: authUser._id,
 				},
