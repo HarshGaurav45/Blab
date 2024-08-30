8a27ba8b47bbce97b3f89e256ec1ea7b905d4f3b
Harsh Gaurav <harshgaurav17@gmail.com>
2024-08-31 01:51:55 +0530

    commit

--- index.js
+++ index.js
@@ -6,18 +6,24 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
+import cors from "cors";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
-
+app.use(cors());
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+
+// Middleware to set cache control headers for static assets
+app.use((req, res, next) => { 
+  res.setHeader('Cache-Control', 'no-store, must-revalidate'); 
+  next(); 
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
